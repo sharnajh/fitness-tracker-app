89e0564f4c53650d9f9055590ea4f101bfe79f2d
Sharna Hossain <sharnajhossain@gmail.com>
2020-01-05 12:07:54 -0500

    stuff

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons"
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Ionicons name="ios-pizza" color="red" size={100} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
